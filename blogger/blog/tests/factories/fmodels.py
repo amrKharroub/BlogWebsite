5ff1a0491c42ea1b31f3f9fe848b30ac15773eac
amr kharroub <amrkharroub@gmail.com>
2023-09-21 23:39:17 +0300

    finished home page

--- blogger/blog/tests/factories/fmodels.py
+++ blogger/blog/tests/factories/fmodels.py
@@ -2,23 +2,31 @@ import factory
 from django.contrib.auth.models import User
 from blog.models import Post
 
+
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
-        
-    username = "testuser"
-    password = "testpass"
+
+    username = factory.Faker("name")
+    password = "test_password123"
     is_superuser = True
     is_staff = True
-    
-    
+
+
 class PostFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Post
-        
-    title = "whocares"
-    subtitle = "idk"
-    slug = "testblog"
+
+    title = factory.Faker("sentence", nb_words=12)
+    subtitle = factory.Faker("sentence", nb_words=7)
+    slug = factory.Faker("slug")
     author = factory.SubFactory(UserFactory)
-    content = factory.Faker("text")
+
+    def content(self):
+        x = ""
+        FAKER = factory.Faker()
+        for _ in range(5):
+            x += FAKER.paragraph(sentences=30) + "\n"
+        return x
+
     published = True
